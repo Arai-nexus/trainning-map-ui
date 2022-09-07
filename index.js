5aedf3e97ee2c3cdf29b64818e326f1c1b516aa2
nexus-arai <nexus.takuya.arai@gmail.com>
2022-09-07 17:26:06 +0900

    feat: モーダルウィンドウから地図UI作成

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+"use strict";
+{
+  const open = document.getElementById("open");
+  const close = document.getElementById("close");
+  const modal = document.getElementById("modal");
+  const mask = document.getElementById("mask");
+
+  open.addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    mask.classList.remove("hidden");
+  });
+  close.addEventListener("click", function () {
+    modal.classList.add("hidden");
+    mask.classList.add("hidden");
+  });
+  mask.addEventListener("click", function () {
+    modal.classList.add("hidden");
+    mask.classList.add("hidden");
+  });
+}
+
 $(function () {
   //地域を選択
   $(".area_btn").click(function () {
